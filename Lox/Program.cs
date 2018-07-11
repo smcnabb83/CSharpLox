1bcd8f8724c2dd091d6babf2326479cb9f8d0972
Sean McNabb <sean.mcnabb.83@gmail.com>
2018-07-10 21:01:16 -0500

    Revert "Initial Commit"
    This reverts commit 1858bccaa2b8f29865899e1fe9b7446e0ef5fd0b.

--- Lox/Program.cs
+++ Lox/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,81 +8,8 @@ namespace Lox
 {
     class Program
     {
-
-        public static bool hasError = false;
-
         static void Main(string[] args)
         {
-
-            if(args.Length > 1)
-            {
-                Console.WriteLine("Usage: clox [script]");
-            }
-
-            else if (args.Length == 1)
-            {
-                runFile(args[0]);
-            }
-
-            else
-            {
-                runPrompt();
-            }
         }
-
-        private static void runFile(string path)
-        {
-            StreamReader reader = new StreamReader(path);
-            run(reader.ReadToEnd());
-            if(hasError)
-            {
-                //add code for exiting application later
-            }
-        }
-
-        private static void runPrompt()
-        {
-            while (true)
-            {
-                Console.Write("> ");
-                run(Console.ReadLine());
-            }
-        }
-
-        private static void run(string source)
-        {
-            Scanner scanner = new Scanner(source);
-            List<Token> tokens = scanner.scanTokens();
-            Parser parser = new Parser(tokens);
-            Expr expression = parser.parse();
-
-            if (hasError) return;
-
-            Console.WriteLine(new AstPrinter().print(expression));
-        }
-
-        public static void error(Token token, string message)
-        {
-            if(token.type == Token.TokenType.EOF)
-            {
-                report(token.line, "at end", message);
-            }
-            else
-            {
-                report(token.line, $"at {token.lexeme}", message);
-            }
-        }
-
-        public static void error(int line, string message)
-        {
-            report(line, "", message);
-        }
-
-        private static void report(int line, string where, string message)
-        {
-            Console.WriteLine($"[line {line}] Error {where} : {message}");
-            hasError = true;
-        }
-        
     }
 }
