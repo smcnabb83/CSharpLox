3ce957a7cf16a760fead604906d1732c2efd1bb8
Sean McNabb <sean.mcnabb.83@gmail.com>
2018-07-10 21:03:20 -0500

    Revert "Revert "Initial Commit""
    This reverts commit 1bcd8f8724c2dd091d6babf2326479cb9f8d0972.

---
+++ Lox/AstPrinter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lox
+{
+    class AstPrinter : Visitor<string>
+    {
+
+        public string print(Expr expression)
+        {
+            return expression.Accept(this);
+        }
+
+        public string visit_Binary_Expr(Binary expr)
+        {
+            return parenthesize(expr.Operator.lexeme, expr.left, expr.right);
+        }
+
+        public string visit_Grouping_Expr(Grouping expr)
+        {
+            return parenthesize("group", expr.expression);
+        }
+
+        public string visit_Literal_Expr(Literal expr)
+        {
+            if (expr.value == null)
+            {
+                return "nil";
+            }
+            return expr.value.ToString();
+        }
+
+        public string visit_Unary_Expr(Unary expr)
+        {
+            return parenthesize(expr.Operator.lexeme, expr.right);
+        }
+
+        private string parenthesize(String name, params Expr[] exprs)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("(").Append(name);
+            foreach(Expr expression in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expression.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Lox/Scanner.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lox
+{
+    public class Scanner
+    {
+        private string Source;
+        private List<Token> tokens = new List<Token>();
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+
+        private static readonly Dictionary<string, Token.TokenType> keywords = new Dictionary<string, Token.TokenType>()
+        {
+            {"and", Token.TokenType.AND },
+            {"class", Token.TokenType.CLASS },
+            {"else", Token.TokenType.ELSE },
+            {"false", Token.TokenType.FALSE },
+            {"for", Token.TokenType.FOR },
+            {"fun", Token.TokenType.FUN },
+            {"if", Token.TokenType.IF },
+            {"nil", Token.TokenType.NIL },
+            {"or", Token.TokenType.OR },
+            {"print", Token.TokenType.PRINT },
+            {"return", Token.TokenType.RETURN },
+            {"super", Token.TokenType.SUPER },
+            {"this", Token.TokenType.THIS },
+            {"true", Token.TokenType.TRUE },
+            {"var", Token.TokenType.VAR },
+            {"while", Token.TokenType.WHILE }
+        };
+
+        public Scanner(string source)
+        {
+            Source = source;
+        }
+
+        public List<Token> scanTokens()
+        {
+            while (!isAtEnd())
+            {
+                start = current;
+                scanToken();
+            }
+
+            tokens.Add(new Token(Token.TokenType.EOF, "", null, line));
+            return tokens;
+        }
+
+        private void scanToken()
+        {
+            char c = advance();
+            switch (c)
+            {
+                case '(': addToken(Token.TokenType.LEFT_PAREN); break;
+                case ')': addToken(Token.TokenType.RIGHT_PAREN); break;
+                case '{': addToken(Token.TokenType.LEFT_BRACE); break;
+                case '}': addToken(Token.TokenType.RIGHT_BRACE); break;
+                case ',': addToken(Token.TokenType.COMMA); break;
+                case '.': addToken(Token.TokenType.DOT); break;
+                case '-': addToken(Token.TokenType.MINUS); break;
+                case '+': addToken(Token.TokenType.PLUS); break;
+                case ';': addToken(Token.TokenType.SEMICOLON); break;
+                case '*': addToken(Token.TokenType.STAR); break;
+                case '!': addToken(match('=') ? Token.TokenType.BANG_EQUAL : Token.TokenType.BANG); break;
+                case '=': addToken(match('=') ? Token.TokenType.EQUAL_EQUAL : Token.TokenType.EQUAL); break;
+                case '<': addToken(match('=') ? Token.TokenType.LESS_EQUAL : Token.TokenType.LESS); break;
+                case '>': addToken(match('=') ? Token.TokenType.GREATER_EQUAL : Token.TokenType.GREATER); break;
+
+                case '/':
+                    if (match('/'))
+                    {
+                        while(peek() != '\n' && !isAtEnd())
+                        {
+                            advance();
+                        }
+                    }
+                    else
+                    {
+                        addToken(Token.TokenType.SLASH);
+                    }
+                    break;
+                case ' ': break;
+                case '\r':  break;
+                case '\t': break;
+                case '\n': line++; break;
+                case '"': processString(); break;
+
+
+                default:
+                    if (isDigit(c))
+                    {
+                        processNumber();
+                    }
+                    else if (isAlpha(c))
+                    {
+                        processIdentifier();
+                    }
+                    else
+                    {
+                        Lox.Program.error(line, "Unexpected Character");
+                    }
+                    break;
+
+            }
+        }
+
+        private char advance()
+        {
+            current++;
+            return Source[current-1];
+        }
+
+        private Boolean match(char expected)
+        {
+            if (isAtEnd())
+            {
+                return false;
+            }
+
+            if(Source[current] != expected)
+            {
+                return false;
+            }
+
+            current++;
+            return true;
+        }
+
+        private void processString()
+        {
+            while(peek() != '"' && !isAtEnd())
+            {
+                if(peek() == '\n')
+                {
+                    line++;
+                }
+                advance();
+            }
+
+            if (isAtEnd())
+            {
+                Program.error(line, "Unterminated String");
+                return;
+            }
+
+            //process closing "
+            advance();
+            string value = Source.Substring(start , current - start);
+            addToken(Token.TokenType.STRING, value);
+        }
+
+        private bool isDigit(char c)
+        {
+            return "0123456789".Contains(c);
+        }
+
+        private void processNumber()
+        {
+            while (isDigit(peek()))
+            {
+                advance();
+            }
+
+            if(peek() == '.' && isDigit(peekNext()))
+            {
+                advance();
+                while (isDigit(peek()))
+                {
+                    advance();
+                }
+            }
+
+            addToken(Token.TokenType.NUMBER, double.Parse(Source.Substring(start, current - start)));
+        }
+
+        private void processIdentifier()
+        {
+            while (isAlphaNumeric(peek()))
+            {
+                advance();
+            }
+            string text = Source.Substring(start, current - start);
+            Token.TokenType type = keywords.ContainsKey(text) ? keywords[text] : Token.TokenType.IDENTIFIER;
+
+            addToken(type);
+        }
+
+        private bool isAlpha(char c)
+        {
+            return "abcdefghijklmnopqrstuvwxyz_".Contains(c.ToString().ToLower());
+        }
+
+        private bool isAlphaNumeric(char c)
+        {
+            return isAlpha(c) || isDigit(c);
+        }
+
+        private char peek()
+        {
+            if (isAtEnd())
+            {
+                return '\0';
+            }
+            return Source[current];
+        }
+
+        private char peekNext()
+        {
+            if (current + 1 >= Source.Length)
+            {
+                return '\0';
+            }
+            return Source[current + 1];
+        }
+
+        private void addToken(Token.TokenType type)
+        {
+            addToken(type, null);
+        }
+
+        private void addToken(Token.TokenType type, Object literal)
+        {
+            string text = Source.Substring(start, current - start);
+            tokens.Add(new Token(type, text, literal, line));
+        }
+
+        private Boolean isAtEnd()
+        {
+            return current >= Source.Length;
+        }
+    }
+}
